9b2333cd9a8668cf7656cd319a5b9c9c5a94a14f
Jeremy <jeremy.pasquiet.e4v@outlook.fr>
2022-06-23 13:47:35 +0200

    ajout CRC, avant modif if wifi_available == true

--- main/main.c
+++ main/main.c
@@ -8,7 +8,7 @@
 #include <freertos/timers.h>
 
 #include "apsta_shared.h"
-
+#include "simplecrc.h"
 #include <driver/gpio.h>
 
 #include <esp_task_wdt.h>
@@ -29,7 +29,7 @@ void xtask_wifi_scanning(void *args)
 			//printf("\n\n\n ############## wifi is initalised, so I can scann ######################### \n\n");
 			scann_wifi_around();
 		}
-			vTaskDelay(15000 / portTICK_PERIOD_MS);
+			vTaskDelay(20000 / portTICK_PERIOD_MS);
 			esp_wifi_disconnect();
 			esp_wifi_stop();
 			vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -208,6 +208,25 @@ void app_main(void)
 
 		printf("-->get SSID = %s\n", my_struct.SSID);
 		printf("-->get PASS = %s \n\n", my_struct.PASS);
+
+
+		uint32_t crc = 0;
+		char jrm[] = "Jeremy";
+		int len = strlen(jrm);
+		crc32(jrm, (len>0)?(len):0, &crc);
+		printf("################################\n");
+		printf("################################\n");
+		printf("Ref for %s = %x\n size is %d \n", jrm, crc, len);
+		printf("################################\n");
+		uint32_t crc2 = 0;
+		// char calculated[strlen(my_struct.SSID)];
+		int len2 = strlen(my_struct.SSID);
+		crc32(my_struct.SSID, (len2>0)?(len2):0, &crc2);
+		// memcpy(my_struct.SSID, my_struct.SSID,len2);
+		printf("Result got with %s = %x\n size is %d\n", my_struct.SSID, crc2, len2);
+		printf("################################\n");
+		printf("################################\n");
+
 
 		switch (err2)
 		{
